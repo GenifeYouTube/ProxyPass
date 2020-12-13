ab1ee04c4433491682e34a90654c8a19bcb0aba5
YaDevelobster <63073463+YaDevelobster@users.noreply.github.com>
2020-12-13 17:21:16 +0500

    test

--- src/main/java/com/nukkitx/proxypass/network/ProxyBedrockEventHandler.java
+++ src/main/java/com/nukkitx/proxypass/network/ProxyBedrockEventHandler.java
@@ -1,20 +1,26 @@
 package com.nukkitx.proxypass.network;
 
+
 import com.nukkitx.protocol.bedrock.BedrockPong;
 import com.nukkitx.protocol.bedrock.BedrockServerEventHandler;
 import com.nukkitx.protocol.bedrock.BedrockServerSession;
 import com.nukkitx.proxypass.ProxyPass;
 import com.nukkitx.proxypass.network.bedrock.session.UpstreamPacketHandler;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+
+import br.com.azalim.mcserverping.MCPing;
+import br.com.azalim.mcserverping.MCPingOptions;
+import br.com.azalim.mcserverping.MCPingResponse;
+import br.com.azalim.mcserverping.MCPingResponse.Description;
+import br.com.azalim.mcserverping.MCPingResponse.Player;
+import br.com.azalim.mcserverping.MCPingResponse.Players;
+import br.com.azalim.mcserverping.MCPingResponse.Version;
+
+
 import java.net.InetSocketAddress;
-import java.net.Socket;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.net.InetSocketAddress;
 
 @Log4j2
 @ParametersAreNonnullByDefault
@@ -25,14 +31,19 @@ public class ProxyBedrockEventHandler implements BedrockServerEventHandler {
 
 
 
-    static {
+
+static {
         ADVERTISEMENT.setEdition("MCPE");
         ADVERTISEMENT.setGameType("Survival");
         ADVERTISEMENT.setVersion(ProxyPass.MINECRAFT_VERSION);
         ADVERTISEMENT.setProtocolVersion(ProxyPass.PROTOCOL_VERSION);
         ADVERTISEMENT.setMotd("hardcore-servers.net");
-        ADVERTISEMENT.setPlayerCount(0);
-        ADVERTISEMENT.setMaximumPlayerCount(100);
+    try {
+        ADVERTISEMENT.setPlayerCount(get());
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    ADVERTISEMENT.setMaximumPlayerCount(100);
         ADVERTISEMENT.setSubMotd("hardcore-servers.net");
     }
 
@@ -56,37 +67,5 @@ public class ProxyBedrockEventHandler implements BedrockServerEventHandler {
     @Override
     public void onSessionCreation(BedrockServerSession session) {
         session.setPacketHandler(new UpstreamPacketHandler(session, this.proxy));
-    }
-    public void getPlayers(){
-        try {
-            @SuppressWarnings("resource")
-            Socket socket = new Socket();
-            socket.connect(new InetSocketAddress("91.224.96.197", 25565), 1 * 1000);
-
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-
-            out.write(0xFE);
-
-            StringBuilder str = new StringBuilder();
-
-            int b;
-            while ((b = in.read()) != -1) {
-                if (b != 0 && b > 16 && b != 255 && b != 23 && b != 24) {
-                    str.append((char) b);
-                }
-            }
-
-            String[] data = str.toString().split("§");
-            int onlinePlayers = Integer.valueOf(data[1]);
-            int maxPlayers = Integer.valueOf(data[2]);
-            String motd = String.valueOf(data[0]);
-            
-            ADVERTISEMENT.setPlayerCount(onlinePlayers);
-
-
-        } catch (Exception evt) {
-            evt.printStackTrace();
-        }
     }
 }
